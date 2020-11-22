7321dfd319f5bb149c176cebc52c631c4bbe1f0b
Muaaz Saleem <muhammad.muaaz.saleem@zalando.de>
2020-11-22 21:52:54 +0100

    Monitoring a single service

--- monit/monit.go
+++ monit/monit.go
@@ -1,7 +1,10 @@
 package monit
 
 import (
+	"net/http"
 	"time"
+
+	log "github.com/sirupsen/logrus"
 )
 
 // UserHTTPTest specifies HTTP tests defined by the user
@@ -12,26 +15,38 @@ type UserHTTPTest struct {
 	// UserHTTPTest only supports "GET" for the moment
 	// Method string `json:"method"`
 
-	// Status defines the expected HTTP status
-	Status int `json:"status"`
+	// StatusCode defines the expected HTTP status
+	StatusCode int `json:"status"`
 	// Interval defines the interval at which the test should be run e.g 20s
 	Interval time.Duration `json:"interval"`
 }
 
-type serviceStatus struct {
-	// status indicates the status of a service i.e "UP", "Failing", "Down"
-	status string
-}
+// TODO: make this a enum type
+// ServiceStatus indicates the status of a service i.e "UP", "FAILING", "DOWN"
+type ServiceStatus string
 
-/* TODO:
- * Take in user test struct
- * generate http request
- * receive response
- * match with test
- * report
- * repeat
- */
+const (
+	ServiceUP      = ServiceStatus("UP")
+	ServiceDown    = ServiceStatus("DOWN")
+	ServiceFailing = ServiceStatus("FAILING")
+)
 
-func RunUserTest(ut UserHTTPTest) serviceStatus {
-	return serviceStatus{status: "UP"}
+func RunUserTest(ut UserHTTPTest) ServiceStatus {
+	// TODO: make sure ut.URL is actually a valid URL
+	res, err := http.Get(ut.URL)
+	// expecting the HTTP client to err out if the service is down
+	// TODO: check or specific errors e.g DNS not resolved
+	if err != nil {
+		// TODO: add TestURL to the log
+		log.Errorf("%s, is 'DOWN'", ut.URL)
+		return ServiceDown
+	}
+
+	if res.StatusCode == ut.StatusCode {
+		log.Infof("%s, is 'UP'", ut.URL)
+		return ServiceUP
+	} else {
+		log.Errorf("%s, is 'FAILING'", ut.URL)
+		return ServiceFailing
+	}
 }
--- monit/monit_test.go
+++ monit/monit_test.go
@@ -1,20 +1,51 @@
 package monit
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestStatusOK(t *testing.T) {
-	ut := UserHTTPTest{
-		URL:      "http://localhost:8080",
-		Status:   200,
-		Interval: time.Minute,
-	}
+var exampleUserTest = UserHTTPTest{
+	URL:        "http://localhost:8080",
+	StatusCode: 200,
+	Interval:   time.Minute,
+}
 
-	expectedStatus := serviceStatus{status: "Down"}
-	serviceStatus := RunUserTest(ut)
-	require.Equal(t, expectedStatus, serviceStatus)
+func TestStatusDown(t *testing.T) {
+	ss := RunUserTest(exampleUserTest)
+	require.Equal(t, ServiceDown, ss)
+}
+
+func TestStatusUP(t *testing.T) {
+	sv := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			_, _ = fmt.Fprint(w, "OK")
+		}))
+	defer sv.Close()
+
+	// point the test endpoint to the test server
+	ut := exampleUserTest
+	ut.URL = sv.URL
+
+	ss := RunUserTest(ut)
+	require.Equal(t, ServiceUP, ss)
+}
+
+func TestStatusFailing(t *testing.T) {
+	sv := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			w.WriteHeader(http.StatusInternalServerError)
+		}))
+	defer sv.Close()
+
+	ut := exampleUserTest
+	ut.URL = sv.URL
+
+	ss := RunUserTest(ut)
+	require.Equal(t, ServiceFailing, ss)
 }
